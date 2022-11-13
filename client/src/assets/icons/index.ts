9e3914598bc69171ff241cefe8a6c9083f417fa2
hopak-e <a328028@gmail.com>
2022-11-13 23:37:40 +0900

    📝 images 관리하는 index 파일 추가

--- client/src/assets/icons/index.ts
+++ client/src/assets/icons/index.ts
@@ -1,1 +1,4 @@
-export {};
+export { default as DecemberIcon } from './DecemberIcon.png';
+export { default as Logo } from './logo.png';
+export { default as AccountIcon } from './AccountIcon.svg';
+export { default as UserIcon } from './UserIcon.svg';
