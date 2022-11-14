9fc813478d7c9e3e6544fc7a6a0e6587364a13bf
hopak-e <a328028@gmail.com>
2022-11-15 04:31:55 +0900

    📝 local storage에 name 값 추가 및 로그인 성공시 루트 페이지로 이동

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,22 +1,27 @@
-import { DecemberIcon } from "@src/assets/icons";
-import { signin } from "@src/core/apis/auth";
-import { ACCESS_TOKEN_KEY } from "@src/core/apis/common";
-import { setLocalStorage } from "@src/utils/storage";
-import { useRef } from "react";
+import { DecemberIcon } from '@src/assets/icons';
+import { signin } from '@src/core/apis/auth';
+import { ACCESS_TOKEN_KEY, USER_NAME } from '@src/core/apis/common';
+import { setLocalStorage } from '@src/utils/storage';
+import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 	const emailInputRef = useRef() as React.MutableRefObject<HTMLInputElement>;
 	const passwordInputRef = useRef() as React.MutableRefObject<HTMLInputElement>;
 
+	const navigate = useNavigate();
+
 	const onLoginFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 
-		const payload = await signin(emailInputRef.current.value, passwordInputRef.current.value)
+		const payload = await signin(emailInputRef.current.value, passwordInputRef.current.value);
 		setLocalStorage(ACCESS_TOKEN_KEY, payload.accessToken);
-	}
+		setLocalStorage(USER_NAME, payload.user.name);
+		navigate('/');
+	};
 
 	return (
-		<div className="flex flex-col justify-center items-center h-screen bg-gray-200">
+		<div className="flex flex-col justify-center items-center h-full bg-gray-200">
 			<div className="w-1/2">
 				<header className="flex justify-center items-center pb-10">
 					<img src={DecemberIcon} className="w-20" />
@@ -25,10 +30,10 @@ const Login = () => {
 				<form onSubmit={onLoginFormSubmit} className=" flex flex-col bg-white border border-black">
 					<p className="flex px-8 py-5 bg-gray-100">로그인</p>
 					<div className="flex py-3 mx-11 my-5 border border-black w-5/6">
-						<input ref={emailInputRef} type="email" placeholder="이메일을 입력하세요" className="pl-5" />
+						<input ref={emailInputRef} type="email" placeholder="이메일을 입력하세요" className="pl-5 outline-none" />
 					</div>
 					<div className="flex  py-3  mx-11 my-5 border border-black w-5/6">
-						<input ref={passwordInputRef} type="password" placeholder="비밀번호를 입력하세요" className="pl-5" />
+						<input ref={passwordInputRef} type="password" placeholder="비밀번호를 입력하세요" className="pl-5 outline-none" />
 					</div>
 					<div className="pt-5 pb-12">
 						<button type="submit" className="w-5/6 mx-11 py-3 bg-gray-100">
