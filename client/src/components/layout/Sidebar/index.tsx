d99a1036a0049b3a4b4511a1d89ad8504783789b
youngmin wi <youngminieo1005@gmail.com>
2022-11-14 22:28:39 +0900

    :sparkles: Base Layout Components 구조 변경

--- client/src/components/layout/Sidebar/index.tsx
+++ client/src/components/layout/Sidebar/index.tsx
@@ -1,4 +1,5 @@
-import { Logo, AccountIcon, UserIcon } from '../../../assets/icons';
+import { Link } from 'react-router-dom';
+import { Logo, AccountIcon, UserIcon } from '@src/assets/icons';
 
 const Sidebar = () => {
 	return (
@@ -6,14 +7,14 @@ const Sidebar = () => {
 			<header className="flex justify-center items-center">
 				<img src={Logo} className="h-20 bg-[#041527]" />
 			</header>
-			<button className="flex items-center text-1xl text-white font-medium px-9 pt-10 pb-5">
+			<Link to="/accounts" className="flex items-center text-1xl text-white font-medium px-9 pt-10 pb-5">
 				<img src={AccountIcon} className="w-6" />
 				<p className="text-3xl font-light pl-5">계좌 목록</p>
-			</button>
-			<button className="flex items-center text-1xl text-white font-medium px-9 pt-10 pb-5">
+			</Link>
+			<Link to="/users" className="flex items-center text-1xl text-white font-medium px-9 pt-10 pb-5">
 				<img src={UserIcon} className="w-6" />
 				<p className="text-3xl font-light pl-5">사용자</p>
-			</button>
+			</Link>
 		</div>
 	);
 };
