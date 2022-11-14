789732bdcebc21514862e5b28cc658c6c13e7afc
seungmin lim <seungmin3238@gmail.com>
2022-11-15 02:06:39 +0900

    🚑 컨플릭트 해결

--- client/src/pages/Board/index.tsx
+++ client/src/pages/Board/index.tsx
@@ -1,27 +1,7 @@
-<<<<<<< HEAD
-import { Sidebar, Header, Footer } from '../../components';
-import BankAccount from '../BankAccount';
-
-const Dashboard = () => {
-	return (
-		<div className="flex h-screen">
-			<Sidebar />
-			<div className="flex-1 flex-col">
-				<Header />
-				<div className="mt-20 h-[calc(100vh_-_160px)] bg-gray-100">
-					<BankAccount />
-				</div>
-				<Footer />
-			</div>
-		</div>
-=======
 import { Layout } from '@src/components';
 
 const Dashboard = () => {
-	return (
-		<Layout></Layout>
->>>>>>> develop
-	);
+	return <Layout></Layout>;
 };
 
 export default Dashboard;
