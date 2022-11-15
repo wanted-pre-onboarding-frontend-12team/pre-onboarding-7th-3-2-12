03200b76e1452b7af2b208051f0768f04251bd20
youngmin wi <youngminieo1005@gmail.com>
2022-11-16 02:47:36 +0900

    :lipstick: Header 레이아웃 수정 및 Sidebar Path 에 따른 배경 하이라이팅

--- client/src/components/layout/Sidebar/index.tsx
+++ client/src/components/layout/Sidebar/index.tsx
@@ -1,32 +1,53 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Logo, AccountIcon, UserIcon, DashboardIcon, LogoutIcon  } from '@src/assets/icons';
 import { removeLocalStorageItem } from '@src/utils/storage';
 import { ACCESS_TOKEN_KEY } from '@src/core/apis/common';
+import { ROUTE_PATH } from '@src/router/routePath';
 
 const Sidebar = () => {
+	const locator = useLocation();
 	const handleLogoutBtnClick = () => {
 		removeLocalStorageItem(ACCESS_TOKEN_KEY);
 		window.location.reload();
 	}
 
 	return (
-		<div className="flex flex-col w-200px h-screen bg-[#041527]">
-			<header className="flex items-center justify-center">
-				<img src={Logo} className="h-20 bg-[#041527]" />
+		<div className="w-60 h-screen bg-[#041527]">
+			<header className="flex items-center justify-center h-20">
+				<img src={Logo} className="h-14 bg-[#041527]" />
 			</header>
-			<Link to="/" className="flex items-center pt-10 pb-5 font-medium text-white text-1xl px-9">
+			<Link
+				to="/"
+				className={`flex items-center py-6 font-medium text-white px-9 text-1xl ${
+					locator.pathname === ROUTE_PATH.DASHBOARD && 'bg-blue-500'
+				}`}
+			>
 				<img src={DashboardIcon} className="w-6" />
 				<p className="pl-5 text-2xl font-light">대시보드</p>
 			</Link>
-			<Link to="/accounts" className="flex items-center pt-10 pb-5 font-medium text-white text-1xl px-9">
+			<Link
+				to="/accounts"
+				className={`flex items-center py-6 font-medium text-white px-9 text-1xl ${
+					locator.pathname.includes(ROUTE_PATH.ACCOUNTS) && 'bg-blue-500'
+				}`}
+			>
 				<img src={AccountIcon} className="w-6" />
 				<p className="pl-5 text-2xl font-light">계좌 목록</p>
 			</Link>
-			<Link to="/users" className="flex items-center pt-10 pb-5 font-medium text-white text-1xl px-9">
+			<Link
+				to="/users"
+				className={`flex items-center py-6 font-medium text-white px-9 text-1xl ${
+					locator.pathname.includes(ROUTE_PATH.USERS) && 'bg-blue-500'
+				}`}
+			>
 				<img src={UserIcon} className="w-6" />
 				<p className="pl-5 text-2xl font-light">사용자</p>
 			</Link>
-			<button type="button" className="flex items-center pt-10 pb-5 font-medium text-white text-1xl px-9" onClick={handleLogoutBtnClick}>
+			<button
+				type="button"
+				className="flex items-center w-full py-6 font-medium text-white px-9 text-1xl"
+				onClick={handleLogoutBtnClick}
+			>
 				<img src={LogoutIcon} className="w-6" />
 				<p className="pl-5 text-2xl font-light">로그아웃</p>
 			</button>
--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -3,11 +3,11 @@ import { Sidebar, Header, Footer } from '@src/components';
 
 const Layout = (props: PropsWithChildren) => {
   return (
-		<div className="flex h-screen">
+		<div className="flex w-screen h-screen max-h-screen max-w-screen">
 			<Sidebar />
-			<div className="flex-1 flex-col">
+			<div className="flex flex-col w-[calc(100vw_-_240px)] relative">
 				<Header />
-				<div className="mt-20 p-8 h-[calc(100vh_-_160px)] bg-gray-100">
+				<div className="h-[calc(100vh_-_160px)] overflow-scroll bg-gray-100 p-8">
 					{props.children}
 				</div>
 				<Footer />
