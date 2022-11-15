fdec87fa6f64d105e31d47e24bde6372770ee8b6
youngmin wi <youngminieo1005@gmail.com>
2022-11-15 20:38:11 +0900

    :sparkles: Users 페이지 - 관리 유저 목록 렌더링 데이터 변환기 적용

--- client/src/components/feature/UserGridTable/index.tsx
+++ client/src/components/feature/UserGridTable/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import { Button } from '@src/components';
 import { UserResponseDTO } from "@src/types/api";
 import { isValidArray } from "@src/utils/isValidArray";
-import { Link } from "react-router-dom";
+import { nameMasker, phoneNumberMasker, dateISOStringToFullDay, genderCodeToGender } from '@src/core/adminDataConverter';
 
 type Props = {
   tableHeadTrs: string[],
@@ -12,57 +13,66 @@ type Props = {
 
 const UserGridTable = (props: Props) => {
 	return (
-		<ul className={`grid grid-cols-${props.gridCols}`}>
-			{isValidArray(props.tableHeadTrs) && props.tableHeadTrs?.map((tableHeadTr, index) => {
-				return (
-					<li key={index} className="py-2 px-5 border border-solid border-gray-300 bg-[#FAFAFA] text-center">
-						{tableHeadTr}
-					</li>
-				);
-			})}
-			{isValidArray(props.tableBodyList) && props.tableBodyList?.map((tableBodyItem) => {
-				const { id, name, gender_origin, birth_date, email, phone_number, created_at, last_login } = tableBodyItem;
+		<ul
+			className={`grid grid-cols-${props.gridCols}`}
+		>
+			{isValidArray(props.tableHeadTrs) &&
+				props.tableHeadTrs?.map((tableHeadTr, index) => {
+					return (
+						<li
+							key={index}
+							className="flex items-center justify-center py-2 px-5 border border-solid border-gray-300 bg-[#FAFAFA] text-center"
+						>
+							{tableHeadTr}
+						</li>
+					);
+				})}
+			{isValidArray(props.tableBodyList) &&
+				props.tableBodyList?.map((tableBodyItem) => {
+					const { id, name, gender_origin, birth_date, email, phone_number, created_at, last_login } = tableBodyItem;
 
-				return (
-					<React.Fragment key={id}>
-						<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
-							<Link to={`/users/${id}`}>{name}</Link>
-						</li>
-						<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
-							{gender_origin}
-						</li>
-						<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
-							{birth_date}
-						</li>
-						<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
-							{email}
-						</li>
-						<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
-							{phone_number}
-						</li>
-						<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
-							{'⭕️'}
-						</li>
-						<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
-							{'❌'}
-						</li>
-						<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
-							{created_at}
-						</li>
-						<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
-							{'2'}
-						</li>
-						<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
-							{last_login}
-						</li>
-						<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid ">
-							<Button type="button" onClick={() => console.log('삭제')} tclass="rounded-md bg-red-600 text-white px-6 py-1">
-								삭제
-							</Button>
-						</li>
-					</React.Fragment>
-				);
-			})}
+					return (
+						<React.Fragment key={id}>
+							<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
+								<Link to={`/users/${id}`} className="border-b-2 border-black hover:text-blue-500 hover:border-blue-500">
+									{name && nameMasker(name)}
+								</Link>
+							</li>
+							<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
+								{gender_origin && genderCodeToGender(gender_origin)}
+							</li>
+							<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
+								{birth_date && dateISOStringToFullDay(birth_date)}
+							</li>
+							<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
+								{email}
+							</li>
+							<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
+								{phone_number && phoneNumberMasker(phone_number)}
+							</li>
+							<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
+								{'⭕️'}
+							</li>
+							<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
+								{'❌'}
+							</li>
+							<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
+								{created_at && dateISOStringToFullDay(created_at)}
+							</li>
+							<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
+								{'2'}
+							</li>
+							<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid">
+								{last_login && dateISOStringToFullDay(last_login)}
+							</li>
+							<li className="flex items-center justify-center px-4 py-2 text-center bg-white border border-gray-300 border-solid ">
+								<Button type="button" onClick={() => console.log('삭제')} className="px-6 py-1 text-white bg-red-600 rounded-md opacity-80 hover:opacity-100">
+									삭제
+								</Button>
+							</li>
+						</React.Fragment>
+					);
+				})}
 		</ul>
 	);
 };
--- client/src/pages/Users/index.tsx
+++ client/src/pages/Users/index.tsx
@@ -6,20 +6,14 @@ import { useQuery } from "@tanstack/react-query"
 const tableHeadTrs = ['고객명', '성별(코드)', '생년월일', "이메일주소", "휴대폰번호", "혜택 수신동의 여부", "활성화 여부", "가입일", "보유 계좌수", "최근로그인 날짜", ""]
 
 const Users = () => {
-  const { data } = useQuery<UserResponseDTO[]>(['getUsers'], async () =>  getUsers(), {
-    onSuccess: (data: UserResponseDTO[]) => {
-      console.log(data);
-    }
-  });
-
-  
+  const { data } = useQuery<UserResponseDTO[]>(['getUsers'], async () =>  getUsers());
 
   return (
 		<Layout>
 			<UserGridTable
 				tableHeadTrs={tableHeadTrs}
 				tableBodyList={data}
-				gridCols="[minmax(100px,_1fr)_minmax(100px,_1fr)_minmax(150px,_1fr)_minmax(150px,_1fr)_minmax(150px,_1fr)_minmax(150px,_1fr)_minmax(150px,_1fr)_minmax(150px,_1fr)_minmax(150px,_1fr)_minmax(150px,_1fr)_minmax(90px,_1fr)]"
+				gridCols="[minmax(150px,_1fr)_minmax(120px,_1fr)_minmax(150px,_1fr)_minmax(200px,_1fr)_minmax(150px,_1fr)_minmax(180px,_1fr)_minmax(150px,_1fr)_minmax(150px,_1fr)_minmax(150px,_1fr)_minmax(150px,_1fr)_minmax(120px,_1fr)]"
 			/>
 		</Layout>
 	);
