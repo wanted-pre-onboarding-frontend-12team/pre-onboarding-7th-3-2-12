3e31054d3287deaf533c10739486536fe38fedd6
youngmin wi <youngminieo1005@gmail.com>
2022-11-16 05:25:43 +0900

    :sparkles: getUsers API 함수 fullText Search 를 위한 query params 확장

--- client/src/core/apis/user.ts
+++ client/src/core/apis/user.ts
@@ -2,14 +2,14 @@ import { API_PATH, httpMehthod } from '@src/core/apis/common';
 import requester from '@src/core/apis/requester';
 import { UserResponseDTO } from '@src/types/api';
 
-export const getUsers = async () => {
+export const getUsers = async (query: string | null) => {
 	const {
 		user: { users },
 	} = API_PATH;
 
 	const { headers, status, payload } = await requester<UserResponseDTO[]>({
 		method: httpMehthod.GET,
-		url: `${users}`,
+		url: `${users}${query ? `?q=${query}` : ''}`,
 	});
 
 	return payload;
