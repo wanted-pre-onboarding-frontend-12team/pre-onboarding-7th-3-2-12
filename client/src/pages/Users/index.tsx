6340d2fc51be319cbd7679cf71779e8a68f1cef4
youngmin wi <youngminieo1005@gmail.com>
2022-11-16 02:49:31 +0900

    :sparkles: 고객 목록 페이지네이션 적용

--- client/src/pages/Users/index.tsx
+++ client/src/pages/Users/index.tsx
@@ -1,20 +1,46 @@
-import { Layout, UserGridTable } from "@src/components"
-import { getUsers } from "@src/core/apis/user"
+import { Layout, Pagenation, UserGridTable } from "@src/components"
+import { getUsers, getUsersByPagenation } from '@src/core/apis/user';
 import { UserResponseDTO } from "@src/types/api";
 import { useQuery } from "@tanstack/react-query"
+import { useMemo, useState } from "react";
 
-const tableHeadTrs = ['고객명', '성별(코드)', '생년월일', "이메일주소", "휴대폰번호", "혜택 수신동의 여부", "활성화 여부", "가입일", "보유 계좌수", "최근로그인 날짜", ""]
+const tableHeadTrs = [
+	'고객명',
+	'성별(코드)',
+	'생년월일',
+	'이메일주소',
+	'휴대폰번호',
+	'혜택 수신동의 여부',
+	'활성화 여부',
+	'가입일',
+	'보유 계좌수',
+	'최근로그인 날짜',
+	'',
+];
+
+const PAGE_OFFSET = 20;
+const DEFALUT_PAGE = 1;
 
 const Users = () => {
-  const { data } = useQuery<UserResponseDTO[]>(['getUsers'], async () =>  getUsers());
+	const [currentPage, setCurrentPage] = useState(DEFALUT_PAGE);
+	const { data: users } = useQuery<UserResponseDTO[]>(['getUsers'], async () => getUsers());
+  const { data: currentUsers } = useQuery<UserResponseDTO[]>(['getUsersByPagenation', currentPage], async () =>
+		getUsersByPagenation(currentPage, PAGE_OFFSET),
+	);
+	const totalPage = useMemo(() => users && Math.floor(users.length / PAGE_OFFSET), [users]); 
 
+	const handlePagenationChange = (newPage: number) => {
+		setCurrentPage(newPage);
+	}
+	
   return (
 		<Layout>
 			<UserGridTable
 				tableHeadTrs={tableHeadTrs}
-				tableBodyList={data}
-				gridCols="[minmax(150px,_1fr)_minmax(120px,_1fr)_minmax(150px,_1fr)_minmax(200px,_1fr)_minmax(150px,_1fr)_minmax(180px,_1fr)_minmax(150px,_1fr)_minmax(150px,_1fr)_minmax(150px,_1fr)_minmax(150px,_1fr)_minmax(120px,_1fr)]"
+				tableBodyList={currentUsers}
+				gridCols="[minmax(150px,_1fr)_minmax(120px,_1fr)_minmax(150px,_1fr)_minmax(200px,_1fr)_minmax(150px,_1fr)_minmax(180px,_1fr)_minmax(150px,_1fr)_minmax(150px,_1fr)_minmax(150px,_1fr)_minmax(120px,_1fr)_minmax(120px,_1fr)]"
 			/>
+			<Pagenation currentPage={currentPage} totalPage={totalPage ?? DEFALUT_PAGE} onPagenationChange={handlePagenationChange} />
 		</Layout>
 	);
 }
