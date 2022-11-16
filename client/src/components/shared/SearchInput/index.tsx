5f857064ec2b0dcecd01598927a7a0998dbd384a
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-11-16 22:41:12 +0900

    ✨ 계좌 목록 페이지 검색 기능 구현(with. JSON Server FullText Search)
    Co-authored-by: forest-6 <seungmin3238@gmail.com>

--- client/src/components/shared/SearchInput/index.tsx
+++ client/src/components/shared/SearchInput/index.tsx
@@ -1,8 +1,27 @@
-const SearchInput = () => {
+import React, { SetStateAction, useRef } from 'react';
+
+type Props = {
+	setSearch: React.Dispatch<SetStateAction<string>>;
+};
+
+const SearchInput = ({ setSearch }: Props) => {
+	const inputRef = useRef<HTMLInputElement>(null);
+	const searchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (inputRef.current) setSearch(inputRef.current?.value);
+	};
+
 	return (
-		<form className="flex items-center">
-			<input placeholder="검색어를 입력하세요." className="h-10 w-48 px-4 mr-1 border-solid border rounded  outline-none" />
-			<button className="h-10 w-12 rounded bg-[#041527] text-white font-bold">검색</button>
+		<form className="flex items-center" onSubmit={searchSubmit}>
+			<input
+				type="text"
+				ref={inputRef}
+				placeholder="검색어를 입력하세요."
+				className="h-10 w-48 px-4 mr-1 border-solid border rounded  outline-none"
+			/>
+			<button type="submit" className="h-10 w-12 rounded bg-[#041527] text-white font-bold">
+				검색
+			</button>
 		</form>
 	);
 };
