496d5ec8f789e0141106678f0440266bb5202be8
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-11-16 22:44:22 +0900

    ✨ 계좌 목록 페이지 필터링 기능 구현
    Co-authored-by: forest-6 <seungmin3238@gmail.com>

--- client/src/components/shared/DropDown/index.tsx
+++ client/src/components/shared/DropDown/index.tsx
@@ -1,11 +1,24 @@
-const DropDown = ({ list }: any) => {
+import useAccountStore from '@src/zustand/useAccountStore';
+import { Zus_Broker_id, Zus_Active, Zus_Status } from '@src/constants/zustand';
+
+const DropDown = ({ list, type }: any) => {
+	const store = useAccountStore((state) => state);
+
+	const filterType = (e: React.ChangeEvent<HTMLSelectElement>, type: string) => {
+		if (type === 'broker') {
+			store.setBroker(e.currentTarget.value as Zus_Broker_id);
+		} else if (type === 'status') {
+			store.setStatus(e.currentTarget.value as Zus_Status);
+		} else if (type === 'active') {
+			store.setActive(e.currentTarget.value as Zus_Active);
+		}
+		//TODO: 여기다가 currentPage 1로 초기화 하는 로직을 넣으면 되지 않을까 싶다.
+	};
+
 	return (
-		<select
-			className="w-36 h-10 px-3 mx-2 border-solid border rounded outline-none"
-			onChange={(e) => console.log(e.target.value)}
-		>
+		<select className="w-36 h-10 px-3 mx-2 border-solid border rounded outline-none" onChange={(e) => filterType(e, type)}>
 			{list.map((item: any) => {
-				return <option value={item}>{item}</option>;
+				return <option value={Object.keys(item)}>{Object.values(item)}</option>;
 			})}
 		</select>
 	);
