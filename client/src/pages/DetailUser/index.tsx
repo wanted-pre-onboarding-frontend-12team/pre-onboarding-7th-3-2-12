f0475ac68cb9f5e417f7e320b405b167876d3854
hopak-e <a328028@gmail.com>
2022-11-18 03:34:04 +0900

    ✨ 계좌목록 사용자 이름 수정 기능 추가
    Co-authored-by: OD <kg-te@naver.com>

--- client/src/pages/DetailUser/index.tsx
+++ client/src/pages/DetailUser/index.tsx
@@ -1,30 +1,31 @@
 import Layout from '@src/components/layout';
 import { updateUserName } from '@src/core/apis/user';
-import { UserObject, UserResponseDTO, UserSettingResponseDTO } from '@src/types/api';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import React, { useEffect, useRef, useState } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import React, { useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useUserAccounts from '@src/hooks/useUserAccounts';
 import UserInfo from '@src/components/feature/UserInfo';
 import { AccountGrid } from '@src/components';
-import useUser from '@src/hooks/useUser';
-import useUserSetting from '@src/hooks/useUserSetting';
+import useUserInfo from '@src/hooks/useUserInfo';
+import { updateAccountList } from '@src/core/apis/account';
 
 const DetailUser = () => {
-	const [info, setInfo] = useState<UserObject>();
 	const [editMode, setEditMode] = useState<boolean>(false);
 	const { uuid, id } = useParams<string>();
 	const { userAccounts } = useUserAccounts(Number(id));
-	const { userData } = useUser(uuid as string);
-	const { userSettingData } = useUserSetting(uuid as string);
+	const { userInfoData } = useUserInfo(uuid as string);
 
 	const ref = useRef<HTMLInputElement>(null);
 	const QueryClient = useQueryClient();
 	const updateName = useMutation(updateUserName, {
-		onSuccess: () =>
+		onSuccess: () => {
 			QueryClient.invalidateQueries(['getUser'], {
 				refetchType: 'all',
-			}),
+			});
+			QueryClient.invalidateQueries(['getUserAccounts'], {
+				refetchType: 'all',
+			});
+		},
 	});
 
 	const onEdit = () => {
@@ -32,19 +33,19 @@ const DetailUser = () => {
 	};
 
 	const onSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
-		if (userData && ref && ref.current) {
-			const id = userData[0].id;
+		const res = (name: string) => {
+			if (userAccounts) {
+				userAccounts.map((el) => updateAccountList(el.id, name));
+			}
+		};
+		if (userInfoData && ref && ref.current) {
+			const id = userInfoData.id;
 			const name = ref.current.value;
 			updateName.mutate({ id, name });
+			res(name);
 			setEditMode((prev) => !prev);
 		}
 	};
-
-	useEffect(() => {
-		if (userData && userSettingData) {
-			setInfo(Object.assign({}, ...userSettingData, ...userData));
-		}
-	}, [userData, userSettingData]);
 
 	return (
 		<Layout>
@@ -68,7 +69,7 @@ const DetailUser = () => {
 						)}
 					</div>
 				</header>
-				{info && <UserInfo info={info} editMode={editMode} ref={ref} />}
+				{userInfoData && <UserInfo info={userInfoData} editMode={editMode} ref={ref} />}
 				<h1 className="text-2xl pt-8"> 계좌 목록</h1>
 				{userAccounts && <AccountGrid accountList={userAccounts} />}
 			</section>
