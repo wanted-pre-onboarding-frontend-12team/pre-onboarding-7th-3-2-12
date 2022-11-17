c5baf48e4b517213c8bf27d443ef88567dfa4575
hopak-e <a328028@gmail.com>
2022-11-17 23:44:26 +0900

    💄 로그인 페이지 ui 수정

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -21,7 +21,7 @@ const Login = () => {
 	};
 
 	return (
-		<div className="flex flex-col justify-center items-center h-full bg-gray-200">
+		<div className="flex flex-col justify-center items-center h-screen bg-gray-200">
 			<div className="w-1/2">
 				<header className="flex justify-center items-center pb-10">
 					<img src={DecemberIcon} className="w-20" />
@@ -29,21 +29,24 @@ const Login = () => {
 				</header>
 				<form onSubmit={onLoginFormSubmit} className=" flex flex-col bg-white border border-black">
 					<p className="flex px-8 py-5 bg-gray-100">로그인</p>
-					<div className="flex py-3 mx-11 my-5 border border-black w-5/6">
-						<input ref={emailInputRef} type="email" placeholder="이메일을 입력하세요" className="pl-5 outline-none w-full" />
-					</div>
-					<div className="flex  py-3  mx-11 my-5 border border-black w-5/6">
-						<input
-							ref={passwordInputRef}
-							type="password"
-							placeholder="비밀번호를 입력하세요"
-							className="pl-5 outline-none w-full"
-						/>
-					</div>
-					<div className="pt-5 pb-12">
-						<button type="submit" className="w-5/6 mx-11 py-3 bg-gray-100">
-							로그인
-						</button>
+					<div className="flex flex-col items-center">
+						<div className="py-3 justify-center my-5 border border-black w-5/6">
+							<input ref={emailInputRef} type="email" placeholder="이메일을 입력하세요" className="pl-5 outline-none w-full" />
+						</div>
+						<div className="flex justify-center py-3  my-5 border border-black w-5/6">
+							<input
+								ref={passwordInputRef}
+								type="password"
+								placeholder="비밀번호를 입력하세요"
+								className="pl-5 outline-none w-full"
+							/>
+						</div>
+
+						<div className="pt-5 pb-12 w-5/6 ">
+							<button type="submit" className="py-3 w-full bg-gray-100">
+								로그인
+							</button>
+						</div>
 					</div>
 				</form>
 				<footer className="flex justify-center py-5 ">Copyright © December and Company</footer>
