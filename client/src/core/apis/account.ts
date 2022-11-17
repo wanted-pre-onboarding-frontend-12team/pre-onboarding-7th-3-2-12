a4f6325a25ac450350e7a02fe783c4a5c6a6b349
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-11-17 17:57:50 +0900

    :ambulance: 계좌 목록 페이지 드롭다운 필터링 오류 해결
    Co-authored-by: forest-6 <seungmin3238@gmail.com>

--- client/src/core/apis/account.ts
+++ client/src/core/apis/account.ts
@@ -2,33 +2,32 @@ import { httpMehthod, API_PATH } from '@src/core/apis/common';
 import requester from '@src/core/apis/requester';
 import { AccountResponseDTO } from '@src/types/api';
 
-export const getAccouts = async (
-	currentPage: number,
-	selectedBroker: string,
-	selectedActive: string,
-	selectedStatus: string,
-	search: string,
-) => {
+type Props = {
+	accountBroker: string;
+	accountStatus: string;
+	accountActive: string;
+};
+
+export const getAccounts = async (currentPage: number, search: string, accountFilterObj: Props) => {
 	const {
 		user: { accounts },
 	} = API_PATH;
 
 	const { headers, status, payload } = await requester<AccountResponseDTO[]>({
 		method: httpMehthod.GET,
-		url: `${accounts}?_page=${currentPage}&_limit=14&broker_id_like=${selectedBroker}&status_like=${selectedStatus}&is_active_like=${selectedActive}&q=${search}`,
+		url: `${accounts}?_page=${currentPage}&q=${search}&_limit=14&broker_id_like=${accountFilterObj.accountBroker}&status_like=${accountFilterObj.accountStatus}&is_active_like=${accountFilterObj.accountActive}`,
 	});
-
 	return payload;
 };
 
-export const getAllAccouts = async (selectedBroker: string, selectedActive: string, selectedStatus: string, search: string) => {
+export const getAllAccounts = async (search: string, accountFilterObj: Props) => {
 	const {
 		user: { accounts },
 	} = API_PATH;
 
 	const { headers, status, payload } = await requester<AccountResponseDTO[]>({
 		method: httpMehthod.GET,
-		url: `${accounts}?q=${search}&broker_id_like=${selectedBroker}&status_like=${selectedStatus}&is_active_like=${selectedActive}`,
+		url: `${accounts}?&q=${search}&broker_id_like=${accountFilterObj.accountBroker}&status_like=${accountFilterObj.accountStatus}&is_active_like=${accountFilterObj.accountActive}`,
 	});
 
 	return payload;
